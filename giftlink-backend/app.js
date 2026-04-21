0735564fb219215be0a82ee7bb25ee3a5c01b557
zwivhuya ratshivhanda <ratshivhanda72@gmail.com>
2026-04-21 05:54:56 -0400

    adding new files

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -1,52 +1,50 @@
 /*jshint esversion: 8 */
 require('dotenv').config();
-
 const express = require('express');
 const cors = require('cors');
+const pinoLogger = require('./logger');
+
+const connectToDatabase = require('./models/db');
+const {loadData} = require("./util/import-mongo/index");
+
+
+const app = express();
+app.use("*",cors());
+const port = 3060;
+
+// Connect to MongoDB; we just do this one time
+connectToDatabase().then(() => {
+    pinoLogger.info('Connected to DB');
+})
+    .catch((e) => console.error('Failed to connect to DB', e));
+
+
+app.use(express.json());
+
+// Route files
+const giftRoutes = require('./routes/giftRoutes');
+const authRoutes = require('./routes/authRoutes');
+const searchRoutes = require('./routes/searchRoutes');
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
 
-const connectToDatabase = require('./models/db');
-const { loadData } = require("./util/import-mongo/index");
-
-// Initialize app
-const app = express();
-const port = 3060;
-
-// Middleware
-app.use(cors());
-app.use(express.json());
 app.use(pinoHttp({ logger }));
-
-// Connect to MongoDB
-connectToDatabase()
-  .then(() => {
-    logger.info('Connected to DB');
-  })
-  .catch((e) => {
-    console.error('Failed to connect to DB', e);
-  });
-
-// Routes
-const giftRoutes = require('./routes/giftRoutes');
-const searchRoutes = require('./routes/searchRoutes');
 
 // Use Routes
 app.use('/api/gifts', giftRoutes);
+app.use('/api/auth', authRoutes);
 app.use('/api/search', searchRoutes);
-
-// Root route
-app.get("/", (req, res) => {
-  res.send("Inside the server");
-});
 
 // Global Error Handler
 app.use((err, req, res, next) => {
-  console.error(err);
-  res.status(500).send('Internal Server Error');
+    console.error(err);
+    res.status(500).send('Internal Server Error');
 });
 
-// Start server
+app.get("/",(req,res)=>{
+    res.send("Inside the server")
+})
+
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+    console.log(`Server running on port ${port}`);
 });
